79c7ebca326bd114f584ac8a025c6ff80688105b
Anuragp22 <anuragp2003b@gmail.com>
2024-10-08 18:24:58 +0530

    Image Upload

---
+++ BACKEND/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import ImageKit from 'imagekit';
+import cors from 'cors';
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
+
+const imagekit = new ImageKit({
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+});
+
+app.get('/api/upload', (req, res) => {
+  var result = imagekit.getAuthenticationParameters();
+  console.log(result);
+  res.send(result);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- CLIENT/src/components/newPrompt/NewPrompt.jsx
+++ CLIENT/src/components/newPrompt/NewPrompt.jsx
@@ -1,8 +1,15 @@
+import Upload from '../upload/upload';
 import './newPrompt.css';
-import { useEffect, useRef } from 'react';
-
+import { useState, useEffect, useRef } from 'react';
+import { IKImage } from 'imagekitio-react';
 const NewPrompt = () => {
   const endRef = useRef(null);
+
+  const [img, setImg] = useState({
+    isLoading: false,
+    error: '',
+    dbData: {},
+  });
 
   useEffect(() => {
     endRef.current.scrollIntoView({ behavior: 'smooth' });
@@ -10,12 +17,19 @@ const NewPrompt = () => {
 
   return (
     <>
+      {img.isLoading && <div className=''>Loading...</div>}
+      {img.dbData?.filePath && (
+        <IKImage
+          urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+          path={img.dbData?.filePath}
+          width='380'
+          transformation={[{ width: 380 }]}
+        />
+      )}
       <div className='endChat' ref={endRef}></div>
       <div className='newForm'>
         <form>
-          <label htmlFor='file'>
-            <img src='/attachment.png' alt='' />
-          </label>
+          <Upload setImg={setImg} />
           <input id='file' type='file' multiple={false} hidden />
           <input type='text' placeholder='Ask any thing...' />
           <button>
