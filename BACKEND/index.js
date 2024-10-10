59de4199b6b4ba487d63e0d4fb409cb1cf467fa8
Anuragp22 <anuragp2003b@gmail.com>
2024-10-10 23:26:41 +0530

    models

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import ImageKit from 'imagekit';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 const port = process.env.PORT || 3000;
 
@@ -11,6 +12,16 @@ app.use(
     origin: process.env.CLIENT_URL,
   })
 );
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB connection successful');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
+    process.exit(1); // Exit process if connection fails
+  }
+};
 
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT,
@@ -25,5 +36,6 @@ app.get('/api/upload', (req, res) => {
 });
 
 app.listen(port, () => {
+  connectDB();
   console.log(`Server is running on port ${port}`);
 });
